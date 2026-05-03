262ee2865e52edd53ef61a48a8e1486f464b534b
Misael97001 <chiluisajefferson97@gmail.com>
2026-05-03 14:23:55 -0500

    Getters and setters, y metodo generar Codigo

--- src/com/krakedev/artesanal/NegocioMejorado.java
+++ src/com/krakedev/artesanal/NegocioMejorado.java
@@ -1,5 +1,27 @@
 package com.krakedev.artesanal;
 
-public class NegocioMejorado {
+import java.util.ArrayList;
 
+public class NegocioMejorado {
+	
+    private ArrayList<Maquina> maquinas;
+
+    public NegocioMejorado() {
+        this.maquinas = new ArrayList<>();
+    }
+    
+    // Getters y Setters
+
+	public ArrayList<Maquina> getMaquinas() {
+		return maquinas;
+	}
+
+	public void setMaquinas(ArrayList<Maquina> maquinas) {
+		this.maquinas = maquinas;
+	}
+    
+	public String generarCodigo() {
+	    int randomNum = (int) (Math.random() * 100) + 1;
+	    return "M-" + randomNum;
+	}
 }
